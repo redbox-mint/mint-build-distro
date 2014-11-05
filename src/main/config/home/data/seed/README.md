443b249ecd9175d8b790bfefd055fd4cda2f7bf2
Shilo Banihit <shilo@redboxresearchdata.com.au>
2014-11-05 14:18:24 +1000

    Added pre-packaged storage and fascinator index with FOR, SEO, Languages and MARC_Countries data.

---
+++ src/main/config/home/data/seed/README.md
@@ -0,0 +1,10 @@
+Seed Data
+=================
+Compressed solr/indexes/fascinator/index and storage directories containing the following:
+
+* FOR
+* SEO
+* Languages
+* MARC_Countries
+
+These files are uncompressed into the proper directories during RPM installation.
--- support/rpm/rpm-build-postinst.sh
+++ support/rpm/rpm-build-postinst.sh
@@ -5,5 +5,9 @@ chown root:root /etc/init.d/mint
 chmod 755 /etc/init.d/mint
 
 chkconfig --level 445 mint on
-
+# existence of storage directory means we have existing data, otherwise copy 'seed' data
+if [ -d /opt/mint/storage ]; then
+  unzip /opt/mint/home/data/seed/storage.zip -d /opt/mint/
+  unzip /opt/mint/home/data/seed/fascinator-index.zip -d /opt/mint/solr/indexes/fascinator
+fi
 service mint restart
