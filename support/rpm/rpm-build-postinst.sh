8f80d1fd2f9de9baedc758e67c6bb13416160a85
Shilo Banihit <shilo@redboxresearchdata.com.au>
2014-11-05 17:33:32 +1000

    Changing flag directory

--- support/rpm/rpm-build-postinst.sh
+++ support/rpm/rpm-build-postinst.sh
@@ -5,8 +5,8 @@ chown root:root /etc/init.d/mint
 chmod 755 /etc/init.d/mint
 
 chkconfig --level 445 mint on
-# existence of storage directory means we have existing data, otherwise copy 'seed' data
-if [ ! -d /opt/mint/storage ]; then
+# means we have existing data, otherwise copy 'seed' data
+if [ ! -d /opt/mint/solr/indexes/fascinator/index ]; then
   unzip /opt/mint/home/data/seed/storage.zip -d /opt/mint/
   unzip /opt/mint/home/data/seed/fascinator-index.zip -d /opt/mint/solr/indexes/fascinator
   chown -R redbox:redbox /opt/mint/
