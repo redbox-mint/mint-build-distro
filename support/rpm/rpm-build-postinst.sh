7f130cc745691713bec7cafc9f807ee09cdec2f0
andrewbrazzatti <andrew@redboxresearchdata.com.au>
2015-01-12 12:50:45 +1030

    Changed conditional as the directory doesn't exist in a clean install.

--- support/rpm/rpm-build-postinst.sh
+++ support/rpm/rpm-build-postinst.sh
@@ -6,8 +6,8 @@ chmod 755 /etc/init.d/mint
 
 chkconfig --level 445 mint on
 # means we have existing data, otherwise copy 'seed' data
-NUMFILES=`ls -1 /opt/mint/solr/indexes/fascinator/index/ | wc -l`
-if [ $NUMFILES -eq 2 ]; then
+DIRECTORY=`/opt/mint/solr/indexes/fascinator/index/`
+if [ ! -d "$DIRECTORY" ]; then
   unzip /opt/mint/home/data/seed/storage.zip -d /opt/mint/
   unzip /opt/mint/home/data/seed/fascinator-index.zip -d /opt/mint/solr/indexes/fascinator
   chown -R redbox:redbox /opt/mint/
