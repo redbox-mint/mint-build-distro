c071d28051596551c12cb350da4b21cba7384a5f
Shilo Banihit <shilo@redboxresearchdata.com.au>
2015-03-30 16:10:45 +1000

    Removing installs, yum hangs.

--- support/rpm/rpm-build-postinst.sh
+++ support/rpm/rpm-build-postinst.sh
@@ -8,11 +8,8 @@ chkconfig --level 445 mint on
 # means we have existing data, otherwise copy 'seed' data
 DIRECTORY=`/opt/mint/solr/indexes/fascinator/index/`
 if [ ! -d "$DIRECTORY" ]; then
-  yum -y install unzip
   unzip /opt/mint/home/data/seed/storage.zip -d /opt/mint/
   unzip /opt/mint/home/data/seed/fascinator-index.zip -d /opt/mint/solr/indexes/fascinator
   chown -R redbox:redbox /opt/mint/
 fi
-# install mint-solr-geonames
-yum -y install mint-solr-geonames
 service mint restart
